430e74d4f588e1b469751868920c13ca1c751d94
Mohammed Sadik <msadikjowel@gmail.com>
2023-12-17 01:41:01 +0600

    changes

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,26 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Login = () => {
-    return (
-        <div className="form">
-            <h2>Please login or Back to <a href="/">Home page</a></h2>
-            <form action='/auth/login' method='post'>
-                <div className="input-container">
-                    <label>Email </label>
-                    <input type="text" name="email" placeholder='Email' required />
-                </div>
-                <div className="input-container">
-                    <label>Password </label>
-                    <input type="password" name="pass" placeholder='Password' required />
-                </div>
-                <div className="button-container">
-                    <input type="submit" />
-                </div>
-
-                <p>Don't have an account? <a href="/auth/register"> Sign up</a></p>
-            </form>
+  return (
+    <div className="form">
+      <h2>
+        Please sign up or Back to <a href="/">Home page</a>
+      </h2>
+      <form action="/auth/login" method="post">
+        <div className="input-container">
+          <label>Email </label>
+          <input type="text" name="email" placeholder="Email" required />
         </div>
-    );
-}
+        <div className="input-container">
+          <label>Password </label>
+          <input type="password" name="pass" placeholder="Password" required />
+        </div>
+        <div className="button-container">
+          <input type="submit" />
+        </div>
+
+        <p>
+          Don't have an account? <a href="/auth/register"> Sign up</a>
+        </p>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
